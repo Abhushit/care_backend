89e3ab39c5ec0ca053af1551bf04ed263123c510
Abhushit Chaudhary <chaudhary.abhushit@gmail.com>
2022-09-12 19:10:41 +0545

    changes on user query

--- src/users/queries.js
+++ src/users/queries.js
@@ -6,6 +6,6 @@ const checkEmailExists = "SELECT * FROM users WHERE email LIKE $1";
 
 const getUserByID = 'SELECT * from users WHERE id = $1';
 
-const addUser = 'INSERT INTO users (firstname, lastname, email, password, role, active) VALUES ($1, $2, $3, $4 $5 $6)';
+const addUser = 'INSERT INTO users (firstname, lastname, email, password, role, active) VALUES ($1, $2, $3, $4, $5, $6)';
 
 export default {getUserByID, getUsers, addUser, checkEmailExists};
