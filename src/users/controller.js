93b5fcf33fef96f1a1b1f00e40790a5a0d289d7d
Abhushit Chaudhary <chaudhary.abhushit@gmail.com>
2022-09-12 15:18:20 +0545

    change in user controller3

--- src/users/controller.js
+++ src/users/controller.js
@@ -21,7 +21,7 @@ const getUsers = (req, res) => {
 
 const addUsers = async (req, res) => {
   const { firstname, lastname, email, password, role, active } = req.body;
-  console.log('request users', req);
+  console.log('request users', req.body);
 
   let errors = [];
 
