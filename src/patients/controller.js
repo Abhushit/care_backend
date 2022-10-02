942c95ba53b54d2eabf67a3de0bfe12341a4fe9c
Abhushit Chaudhary <chaudhary.abhushit@gmail.com>
2022-10-02 17:55:02 +0545

    currently

--- src/patients/controller.js
+++ src/patients/controller.js
@@ -327,6 +327,8 @@ const updateFindings = (req,res) => {
   const { findings } = req.body;
   const id = req.params.id;
 
+  console.log('current',`${findings.map(find => find)}`);
+
   pool.query(queries.updateFindings, [
     findings.map(find => find), id
   ], (err, result) => {
